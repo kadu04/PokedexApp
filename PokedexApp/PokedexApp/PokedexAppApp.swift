e54276ba434ef0c40c9462beac884e62c10ebdad
Ricardo Massaki <ricardo29puruba@gmail.com>
2025-05-15 16:38:48 -0300

    Initial commit create new project PokedexApp

---
+++ PokedexApp/PokedexApp/PokedexAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexAppApp.swift
+//  PokedexApp
+//
+//  Created by Ricardo Massaki on 15/05/25.
+//
+
+import SwiftUI
+
+@main
+struct PokedexAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PokedexApp/PokedexAppTests/PokedexAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexAppTests.swift
+//  PokedexAppTests
+//
+//  Created by Ricardo Massaki on 15/05/25.
+//
+
+import Testing
+@testable import PokedexApp
+
+struct PokedexAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
